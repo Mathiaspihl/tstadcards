d17354ea038a3a17a96e07a48475b4be9c7b3c29
Mathiaspihl <mathias.pihl@gmail.com>
2022-12-17 20:00:32 +0100

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/*!
+* Start Bootstrap - Modern Business v5.0.4 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
